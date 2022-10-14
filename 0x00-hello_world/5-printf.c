378679593f601203f91e5908b64c1f155a4b046f
Bunmi <monehinolubunmi1@gmailcom>
2022-10-14 13:00:31 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * * main: print a script
+ * * Description: use printf
+ * * Return: 0
+ * */
+
+int main(void)
+{
+	printf("With proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
