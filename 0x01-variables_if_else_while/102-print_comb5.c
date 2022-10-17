1e0554214e79d12f9691cd5871bef9dc6925204e
Bunmi <monehinolubunmi1@gmailcom>
2022-10-17 14:52:22 -0700

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ ** main - Entry point
+ ** Description: prints combination
+ ** Return: 0 success
+ **/
+
+int main(void)
+{
+		int i, o;
+
+		for (i = 0; i < 100; i++)
+		{
+			for (o = i + 1; o < 100; o++)
+			{
+				putchar ((i / 10) + '0');
+				putchar ((i % 10) + '0');
+				putchar (' ');
+				putchar ((o / 10) + '0');
+				putchar ((o % 10) + '0');
+
+				if (i == 98 && o == 99)
+					continue;
+
+				putchar (',');
+				putchar (' ');
+			}
+		}
+		putchar ('\n');
+		return (0);
+}
+
