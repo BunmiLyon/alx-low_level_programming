970981e2cea3c199a6aac1d20efcacc6a2080089
Bunmi <monehinolubunmi1@gmailcom>
2022-10-17 11:45:12 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main -Entry point
+  * Description: prints numbers till 10
+  * Return: 0 sucess
+  */
+int main(void)
+{
+		int i;
+
+		for (i = 0; i < 10; i++)
+		{
+			printf("%d", i);
+		}
+		printf("\n");
+		return (0);
+}
