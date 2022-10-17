19707e77af011053bee26b92e4e0b723ba545161
Bunmi <monehinolubunmi1@gmailcom>
2022-10-17 11:55:18 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ ** main - Entry point
+ ** Description: all single values in base 10
+ ** Return: 0 success
+ **/
+
+int main(void)
+{
+		int c;
+
+		for (c = 48; c <= 57; c++)
+		{	putchar(c);
+		}
+			putchar('\n');
+		return (0);
+}
