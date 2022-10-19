fed14a36589bc4ee219b9b0c49d7265a003aaec8
Bunmi <monehinolubunmi1@gmailcom>
2022-10-19 13:28:47 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ ** print_alphabet - Entry point
+ **
+ ** Description: Prints lowercase alphabet
+ ** Return: 0 success
+ **/
+void print_alphabet(void)
+{
+		char letter;
+
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+}
