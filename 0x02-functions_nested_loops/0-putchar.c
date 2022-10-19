14330208b10791a4cbd33b166a737d06d8e06cc6
Bunmi <monehinolubunmi1@gmailcom>
2022-10-19 15:34:53 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
  ** main - prints _putchar
  ** Return: 0 success
  **/
 int main(void)
 {
-	char var[] = "_putchar";
-	int i;
-	int lenVar = sizeof(var) / sizeof(var[0]);
-
-	for (i = 0; i < lenVar - 1; i++)
-	{
-		_putchar(var[i]);
-	}
-	_putchar('\n');
-
-	return (0);
+	putchar('_');
+		putchar('p');
+			putchar('u');
+				putchar('t');
+					putchar('c');
+						putchar('h');
+							putchar('a');
+								putchar('r');
+									putchar('\n');
+										return (0);
 }
