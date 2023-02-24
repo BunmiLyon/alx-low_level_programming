a8eb3078d7d6d3b4633c01812973eafa92de7090
Bunmi <monehinolubunmi1@gmailcom>
2023-02-24 00:03:01 -0800

    More functions - More nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - check the code.
+ * @c: variable to be tested
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	if (c == 65)
+		return (1);
+	else if (c == 97)
+		return (0);
+	return (0);
+}
