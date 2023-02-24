a045575ab603ec4b633f7e5454ac193bfdfdb0f2
Bunmi <monehinolubunmi1@gmailcom>
2023-02-24 01:22:23 -0800

    More functions - More nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
-
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: Always 0.
+ */
 void print_most_numbers(void)
 {
 	int a;
@@ -10,7 +14,7 @@ void print_most_numbers(void)
 	for (a = '0'; a <= '9'; a++)
 	{
 		if (a != b  && a != c)
-		       _putchar (a);	
+			_putchar (a);	
 	}
 	_putchar('\n');
 }
