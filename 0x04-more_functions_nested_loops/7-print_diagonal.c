c58dfceb6e9e66fdeba7608f5773cfe696763e8b
Bunmi <monehinolubunmi1@gmailcom>
2023-02-24 15:06:15 -0800

    More functions - More nested loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code
+ * @n: numver of diagonals
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int y, x;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (y = 0; y < n; y++)
+		{
+			for (x = 0; x < y; x++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
