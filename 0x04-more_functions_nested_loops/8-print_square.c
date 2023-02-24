3d687d3ea69452d616b5765dd10cd12e3a6770ec
Bunmi <monehinolubunmi1@gmailcom>
2023-02-24 15:12:07 -0800

    More functions - More nested loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - check the code
+ * @size: size of square
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int a, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
