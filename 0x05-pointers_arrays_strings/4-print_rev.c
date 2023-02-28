cbde5f43d5b8a8a2020f924c89234621a0c0cbd4
Bunmi <monehinolubunmi1@gmailcom>
2023-02-28 03:15:11 -0800

    Pointers, Arrays and Strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - Write a function that prints a string, followed
+ * by a new line, to stdout.
+ * @s: an input string
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	while (s[len] != '\0')
+		len++;
+	while (len)
+		_putchar(s[--len]);
+	_putchar('\n');
+}
