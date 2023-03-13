0c8e48c7b39ff2945b5d279bce49d02e1c8f1a03
Bunmi <monehinolubunmi1@gmailcom>
2023-03-13 08:45:09 -0700

    C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ * @argc: The size of the argv array
+ * @argv: An array of size argc
+ * Return: 0
+ */
+
+int main(int __attribute((__unused__))  argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
