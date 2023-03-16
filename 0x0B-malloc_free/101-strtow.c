b884705118dd38392aa6ccd2f16831b1b5c6a0c7
Bunmi <monehinolubunmi1@gmailcom>
2023-03-16 06:07:52 -0700

    malloc and free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -28,7 +28,9 @@ void ch_free_grid(char **grid, unsigned int height)
 char **strtow(char *str)
 {
 	char **aout;
+
 	unsigned int c, height, i, j, a1;
+
 	if (str == NULL || *str == '\0')
 		return (NULL);
 	for (c = height = 0; str[c] != '\0'; c++)
